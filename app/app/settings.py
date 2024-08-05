65c412d9750f23c25b62fa86abc3c4f93999449b
Guilherme Felix <guilhermerutemfelix@gmail.com>
2024-08-05 19:15:24 -0300

    Alterações página de funcionarios

--- app/app/settings.py
+++ app/app/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'funcionarios',
     'smartwatches',
 ]
@@ -108,7 +109,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- app/funcionarios/forms.py
+++ app/funcionarios/forms.py
@@ -1,8 +1,14 @@
 from django import forms
 from .models import Funcionario
 
-
 class FuncionarioForm(forms.ModelForm):
     class Meta:
         model = Funcionario
         fields = ['nome_completo', 'email', 'idade', 'altura', 'peso']
+        widgets = {
+            'nome_completo': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'idade': forms.NumberInput(attrs={'class': 'form-control'}),
+            'altura': forms.NumberInput(attrs={'class': 'form-control'}),
+            'peso': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
