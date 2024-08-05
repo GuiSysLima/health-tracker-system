177ab4daf4f49d0a59286ae2f4ef556fb4dbae3f
Guilherme Felix <guilhermerutemfelix@gmail.com>
2024-08-05 17:45:10 -0300

    Alterações nos estilos da pagina e configuração de páginas em funcionarios

--- app/funcionarios/urls.py
+++ app/funcionarios/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('cadastrar/', views.cadastrar_funcionario, name='cadastrar_funcionario'),
     path('listar/', views.listar_funcionarios, name='listar_funcionarios'),
+    path('funcionarios/perfil/<int:id>/', views.perfil_funcionario, name='perfil_funcionario'),
 ]
--- app/funcionarios/views.py
+++ app/funcionarios/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import FuncionarioForm
 from .models import Funcionario
 
@@ -13,5 +13,38 @@ def cadastrar_funcionario(request):
     return render(request, 'funcionarios/cadastrar_funcionario.html', {'form': form})
 
 def listar_funcionarios(request):
-    funcionarios = Funcionario.objects.all()
+    search_query = request.GET.get('search', '')
+    search_type = request.GET.get('searchType', 'nome')
+
+    #Lógica de busca de funcionarios
+    if search_query:
+        if search_type == 'nome':
+            funcionarios = Funcionario.objects.filter(nome_completo__icontains=search_query)
+        elif search_type == 'email':
+            funcionarios = Funcionario.objects.filter(email__icontains=search_query)
+        elif search_type == 'idade':
+            try:
+                idade = int(search_query)
+                funcionarios = Funcionario.objects.filter(idade=idade)
+            except ValueError:
+                funcionarios = Funcionario.objects.none()  # Se a idade não for um número, não retorna resultados.
+        elif search_type == 'altura':
+            try:
+                altura = float(search_query)
+                funcionarios = Funcionario.objects.filter(altura=altura)
+            except ValueError:
+                funcionarios = Funcionario.objects.none()  # Se a altura não for um número, não retorna resultados.
+        elif search_type == 'peso':
+            try:
+                peso = float(search_query)
+                funcionarios = Funcionario.objects.filter(peso=peso)
+            except ValueError:
+                funcionarios = Funcionario.objects.none()  # Se o peso não for um número, não retorna resultados.
+    else:
+        funcionarios = Funcionario.objects.all()
+
     return render(request, 'funcionarios/listar_funcionarios.html', {'funcionarios': funcionarios})
+
+def perfil_funcionario(request, id):
+    funcionario = get_object_or_404(Funcionario, id=id)
+    return render(request, 'funcionarios/perfil_funcionario.html', {'funcionario': funcionario})
